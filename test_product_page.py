754ff6cd9246c94e7077a8e9e2f71ebea7d2679f
Николай Секирин <snv031@gmail.com>
2025-05-10 11:49:56 +0300

    Добавление параметризации

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,13 @@
 import pytest
 from .pages.product_page import ProductPage
 
+links = [
+    "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+]
 
-@pytest.mark.parametrize('link', [
-    "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"])
+
+@pytest.mark.parametrize('link', links)
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
